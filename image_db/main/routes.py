f0b54015b3de1549fb34db60974f2d7738776cd0
Florian <florian.abraham@live.com>
2021-12-20 21:20:37 +0100

    Commit initial files for webapp

---
+++ image_db/main/routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from flask import render_template, request
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+@main.route("/home")
+def home():
+    return render_template("home.html")
