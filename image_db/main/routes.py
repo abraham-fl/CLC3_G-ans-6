12741b23e02b8f9434c6f6122a13108903840da9
Florian <florian.abraham@live.com>
2021-12-26 23:06:51 +0100

    Add route to home page of webapp

--- image_db/main/routes.py
+++ image_db/main/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import Blueprint
 from flask import render_template, request
 
@@ -6,4 +7,6 @@ main = Blueprint("main", __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template("home.html")
+    page = request.args.get("page", 1, type=int)
+    images = os.listdir(r"C:\Users\flori\OneDrive\Dokumente\FH_Hagenberg\Studium\DSE\3_Semester\CLC\Projekt\CLC3_G-ans-6\image_db\static\images")
+    return render_template("home.html", images=images)
