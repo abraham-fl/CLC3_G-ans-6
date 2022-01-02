548143d5c52a87f6dc84caa04f618eb4fbd1f61a
Florian <florian.abraham@live.com>
2022-01-02 12:38:39 +0100

    Add filtering via tags before images are shown

--- image_db/main/routes.py
+++ image_db/main/routes.py
@@ -14,12 +14,28 @@ def home():
     page = request.args.get("page", 1, type=int)
     per_page = 6
     blob_images = Db.getImageContainer().list_blobs()
+    count = Counter()
+    for image in blob_images:
+        blob_client = Db.getImageContainer().get_blob_client(blob=image.name)
+        count.update(blob_client.get_blob_properties().metadata["tags"].split(", "))
+
+    count = [i[0] for i in count.most_common()]
+    return render_template('home.html', pagination = None, count = count)
+
+@main.route("/results", methods = ['GET', 'POST'])
+def results():
+    page = request.args.get("page", 1, type=int)
+    tags = [request.args.get("tag1"), request.args.get("tag2"), request.args.get("tag3")]
+    per_page = 6
+    blob_images = Db.getImageContainer().list_blobs()
     images = []
     count = Counter()
     for image in blob_images:
         blob_client = Db.getImageContainer().get_blob_client(blob=image.name)
-        images.append(blob_client)
-        count.update(blob_client.get_blob_properties().metadata["tags"].split(", "))
+        image_tags = blob_client.get_blob_properties().metadata["tags"].split(", ")
+        if any (x in tags for x in image_tags):
+            images.append(blob_client)
+            count.update(image_tags)
 
     count = [i[0] for i in count.most_common()]
     # get the start and end index based on page number
@@ -28,4 +44,4 @@ def home():
     # page 1 is [0:6], page 2 is [6:12]
     items = images[start:end]
     pagination = Pagination(None, page, per_page, len(images), items)
-    return render_template('home.html', pagination=pagination, count = count)
+    return render_template('results.html', pagination=pagination, count = count, tag1 = tags[0], tag2 = tags[1], tag3=tags[2])
