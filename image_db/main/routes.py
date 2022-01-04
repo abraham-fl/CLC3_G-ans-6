e2178df03bd1ee5c5bf3e406e12229f7b88ee9b1
Florian <florian.abraham@live.com>
2022-01-04 16:29:58 +0100

    Reformat routes file

--- image_db/main/routes.py
+++ image_db/main/routes.py
@@ -8,23 +8,31 @@ main = Blueprint("main", __name__)
 
 database = Db()
 
-@main.route("/", methods = ['GET'])
-@main.route("/home", methods = ['GET'])
+
+@main.route("/", methods=["GET"])
+@main.route("/home", methods=["GET"])
 def home():
     blob_images = Db.getImageContainer().list_blobs()
     count = Counter()
     for image in blob_images:
         blob_client = Db.getImageContainer().get_blob_client(blob=image.name)
         if "tags" in blob_client.get_blob_properties().metadata:
-            count.update(set(blob_client.get_blob_properties().metadata["tags"].split(", ")))
+            count.update(
+                set(blob_client.get_blob_properties().metadata["tags"].split(", "))
+            )
 
     count = [i[0] for i in count.most_common()]
-    return render_template('home.html', pagination = None, count = count)
+    return render_template("home.html", pagination=None, count=count)
 
-@main.route("/results", methods = ['GET', 'POST'])
+
+@main.route("/results", methods=["GET", "POST"])
 def results():
     page = request.args.get("page", 1, type=int)
-    tags = [request.args.get("tag1"), request.args.get("tag2"), request.args.get("tag3")]
+    tags = [
+        request.args.get("tag1"),
+        request.args.get("tag2"),
+        request.args.get("tag3"),
+    ]
     reduced_tags = set([t for t in tags if t])
     per_page = 6
     blob_images = Db.getImageContainer().list_blobs()
@@ -33,7 +41,9 @@ def results():
     for image in blob_images:
         blob_client = Db.getImageContainer().get_blob_client(blob=image.name)
         if "tags" in blob_client.get_blob_properties().metadata:
-            image_tags = set(blob_client.get_blob_properties().metadata["tags"].split(", "))
+            image_tags = set(
+                blob_client.get_blob_properties().metadata["tags"].split(", ")
+            )
             if reduced_tags.issubset(image_tags):
                 images.append(blob_client)
                 count.update(image_tags)
@@ -45,4 +55,11 @@ def results():
     # page 1 is [0:6], page 2 is [6:12]
     items = images[start:end]
     pagination = Pagination(None, page, per_page, len(images), items)
-    return render_template('results.html', pagination=pagination, count = count, tag1 = tags[0], tag2 = tags[1], tag3=tags[2])
+    return render_template(
+        "results.html",
+        pagination=pagination,
+        count=count,
+        tag1=tags[0],
+        tag2=tags[1],
+        tag3=tags[2],
+    )
