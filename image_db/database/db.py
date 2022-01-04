392bfd5a0c1fced1930326266cbbe3303f7ed1d8
Florian <florian.abraham@live.com>
2022-01-04 17:04:52 +0100

    Reformat for pep8

--- image_db/database/db.py
+++ image_db/database/db.py
@@ -5,7 +5,7 @@ config = configparser.ConfigParser()
 config.read("imagedb.ini")
 
 
-class Db():
+class Db:
     def __init__(self):
         Db.__BlobConnectionString = config["BlobStorage"]["ConnectionString"]
         Db.__BlobThumbnailContainer = config["BlobStorage"]["ThumbnailContainer"]
@@ -19,22 +19,32 @@ class Db():
 
     def __createBlobConnection():
         if not Db.__BlobConnection:
-            Db.__BlobConnection = BlobServiceClient.from_connection_string(conn_str = Db.__BlobConnectionString)
+            Db.__BlobConnection = BlobServiceClient.from_connection_string(
+                conn_str=Db.__BlobConnectionString
+            )
 
     def __createContainerConnections():
         Db.__createBlobConnection()
 
         try:
-            Db.__ThumbnailConnection = Db.__BlobConnection.get_container_client(container = Db.__BlobThumbnailContainer)
+            Db.__ThumbnailConnection = Db.__BlobConnection.get_container_client(
+                container=Db.__BlobThumbnailContainer
+            )
             Db.__ThumbnailConnection.get_container_properties()
         except:
-            Db.__ThumbnailConnection = Db.__BlobConnection.create_container(Db.__BlobThumbnailContainer)
+            Db.__ThumbnailConnection = Db.__BlobConnection.create_container(
+                Db.__BlobThumbnailContainer
+            )
 
         try:
-            Db.__ImageConnection = Db.__BlobConnection.get_container_client(container = Db.__BlobImageContainer)
+            Db.__ImageConnection = Db.__BlobConnection.get_container_client(
+                container=Db.__BlobImageContainer
+            )
             Db.__ImageConnection.get_container_properties()
         except:
-            Db.__ImageConnection = Db.__BlobConnection.create_container(Db.__BlobImageContainer)
+            Db.__ImageConnection = Db.__BlobConnection.create_container(
+                Db.__BlobImageContainer
+            )
 
     def getThumbnailContainer():
         return Db.__ThumbnailConnection
